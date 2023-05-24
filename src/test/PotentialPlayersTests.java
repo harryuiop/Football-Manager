268e49c9b042e2ef921e38e12e69b1f95f1d8d54
Harrison Ellis <hel46@uclive.ac.nz>
2023-05-24 20:21:51 +1200

    added more tests

--- src/test/PotentialPlayersTests.java
+++ src/test/PotentialPlayersTests.java
@@ -25,11 +25,9 @@ class PotentialPlayersTests {
 
         ArrayList<Athlete> allPlayers = potentialPlayers.getAllPlayers();
 
-        // Check if the total number of players created is correct
-        int expectedPlayerCount = numWeeks * 2 * 6; // 2 teams per week, 6 players per team
+        int expectedPlayerCount = numWeeks * 2 * 6; 
         Assertions.assertEquals(expectedPlayerCount, allPlayers.size());
 
-        // Validate that all players have been created with valid attributes
         for (Athlete ath : allPlayers) {
             Assertions.assertTrue(ath.getOffence() >= 0 && ath.getOffence() <= 100);
             Assertions.assertTrue(ath.getDefence() >= 0 && ath.getDefence() <= 100);
@@ -44,25 +42,19 @@ class PotentialPlayersTests {
     	ArrayList<String> copyBeforeRemoval = new ArrayList<String>();
     	copyBeforeRemoval.addAll(potentialPlayers.getNames());
         String name = potentialPlayers.nameGenerator();
-        // Validate that a name is generated and it is not empty
         Assertions.assertNotNull(name);
         Assertions.assertFalse(name.isEmpty());
-
-        // Validate that the generated name is removed from the names list
         ArrayList<String> names = potentialPlayers.getNames();
         Assertions.assertFalse(names.contains(name));
     }
 
-    // Additional tests for getters and setters
 
     @Test
     void testGetAllPlayers() {
         ArrayList<Athlete> allPlayers = new ArrayList<>();
         allPlayers.add(new Athlete(80, 50, 1, 2, "John"));
         allPlayers.add(new Athlete(60, 30, 2, 1, "Michael"));
-
         potentialPlayers.setAllPlayers(allPlayers);
-
         Assertions.assertEquals(allPlayers, potentialPlayers.getAllPlayers());
     }
 
@@ -71,9 +63,7 @@ class PotentialPlayersTests {
         ArrayList<Athlete> allPlayers = new ArrayList<>();
         allPlayers.add(new Athlete(80, 50, 1, 2, "John"));
         allPlayers.add(new Athlete(60, 30, 2, 1, "Michael"));
-
         potentialPlayers.setAllPlayers(allPlayers);
-
         Assertions.assertEquals(allPlayers, potentialPlayers.getAllPlayers());
     }
 }
